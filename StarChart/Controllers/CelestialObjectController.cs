65ae1ec8305ef555e73e8b97b83254eca15750e7
Christian Chapman <chchapman@microsoft.com>
2022-08-19 17:52:52 -0500

    created get actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,6 +1,9 @@
-﻿using System.Security.AccessControl;
+﻿using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -14,5 +17,52 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        IActionResult GetById(int id)
+        {
+            CelestialObject requestedCelObj = _context.CelestialObjects.Find(id);
+
+            if (requestedCelObj == null) return NotFound();
+
+            foreach (CelestialObject obj in _context.CelestialObjects)
+            {
+                if (obj.OrbitedObjectId == id)
+                {
+                    requestedCelObj.Satellites.Add(obj);
+                }
+            }
+
+            return Ok(requestedCelObj);
+
+        }
+
+        [HttpGet("{name}")]
+        IActionResult GetByName(string name)
+        {
+            CelestialObject requestedCelObj = _context.CelestialObjects.Find(name);
+
+            if (requestedCelObj == null) return NotFound();
+
+            foreach (CelestialObject obj in _context.CelestialObjects)
+            {
+                if (obj.OrbitedObjectId == requestedCelObj.Id)
+                {
+                    requestedCelObj.Satellites.Add(obj);
+                }
+            }
+
+            return Ok(requestedCelObj);
+
+        }
+
+        [HttpGet]
+        IActionResult GetAll()
+        {
+            
+            return Ok();
+            
+        }
+
     }
 }
